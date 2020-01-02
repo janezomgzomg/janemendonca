017dec8864fd2b994abdd837b3185c1de10c974f
Jane Mendonca <jane.zomg@gmail.com>
2020-01-01 16:55:59 -0800

    * pushing change

--- resources/scripts/old_index.js
+++ resources/scripts/old_index.js
@@ -1,22 +1,8 @@
 // import PageHeaderModule from 'components/page-header/page-header.module.js';
 
 function init() {
-  // setDefaultView();
-  // ();
-  // console.log(PageHeaderModule);
 }
 
-// function openLink(link) {
-//   var LINKS = {
-//     github: {
-//       value: 'https://github.com/janezomgzomg'
-//     },
-//     linkedin: {
-//       value: 'https://www.linkedin.com/in/jane-mendonca-ab218a43/'
-//     }
-//   };
-//   var win = window.open(LINKS[link].value, '_blank');
-// }
 
 
 // function setDefaultView() {
