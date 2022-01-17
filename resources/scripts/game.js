61470eb79b75adfd890905902a8ce2fee50d5e6d
janezomgzomg <jane.zomg@gmail.com>
2022-01-17 15:55:00 -0800

    phase 1

--- resources/scripts/game.js
+++ resources/scripts/game.js
@@ -23,6 +23,8 @@ function initScreenObject() {
   SCREEN['element'] = document.getElementById('game-screen');
   var pos = SCREEN.element.getBoundingClientRect();
   SCREEN['x'] = pos.x;
+  SCREEN['top'] = pos.top;
+  SCREEN['bottom'] = pos.bottom;
   SCREEN['left'] = pos.left;
   SCREEN['right'] = pos.right;
 }
@@ -30,6 +32,7 @@ function initScreenObject() {
 function initPlayerObject() {
   PLAYER['element'] = document.getElementById('player');
   PLAYER['width'] = PLAYER.element.getBoundingClientRect().width;
+  PLAYER['height'] = PLAYER.element.getBoundingClientRect().height;
 }
 
 function registerKeyPressEvents() {
@@ -41,6 +44,7 @@ function keydown(event) {
   switch(event.code) {
     case 'ArrowRight': movePlayerRight(); break;
     case 'ArrowLeft': movePlayerLeft(); break;
+    // case 'ArrowUp': jumpPlayer(); break;
   }
 }
 
@@ -64,4 +68,26 @@ function movePlayerLeft() {
   } else {
     player.style.left = SCREEN.right - SCREEN.left - (2* PLAYER.width) + 'px';
   }
+}
+
+function jumpPlayer() {
+  var currentPlayerPos = PLAYER.element.getBoundingClientRect();
+  console.log(SCREEN.bottom);
+  console.log(currentPlayerPos.bottom);
+  var originalPos = currentPlayerPos.bottom - SCREEN.top;
+  var newPos = originalPos - PLAYER.height;
+  console.log(newPos);
+  player.style.bottom = newPos + 'px';
+  // setTimeout(function (){
+  //   player.style.bottom = `calc(${originalPos}px + 0.25em)`;
+  // }, 100);
+  // // var newPos = (currentPlayerPos.x - SCREEN.x) - PLAYER.width;
+
+  // console.log(currentPlayerPos);
+
+  // if((newPos + SCREEN.left) > (SCREEN.left + PLAYER.width)) {
+  //   player.style.left = newPos + 'px';
+  // } else {
+  //   player.style.left = SCREEN.right - SCREEN.left - (2* PLAYER.width) + 'px';
+  // }
 }
